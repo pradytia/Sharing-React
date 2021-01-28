ed54107846a4affbbfae93cba2a8be8f3e5bb793
pradytia <pradytia@mobilecom.co.id>
2021-01-28 17:35:20 +0700

    pertama gan

---
+++ src/component/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Dashboard(){
+
+    
+
+    return(
+        <div>
+            <h1>Ini Halaman Dashboard</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
